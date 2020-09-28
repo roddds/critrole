3cd26beacd6353dfc33c5258746bbd6551dc5363
Rodrigo Deodoro <rodrigo.deodoro@esharesinc.com>
2020-09-27 19:53:07 -0700

    add model string representations

--- episodes/models.py
+++ episodes/models.py
@@ -12,9 +12,15 @@ class Episode(models.Model):
     subtitle_filename = models.TextField(verbose_name="Episode subtitle file name")
     raw_captions = models.TextField(verbose_name="Raw caption contents")
 
+    def __str__(self):
+        return f"{self.chapter} - {self.title}"
+
 
 class CastMember(models.Model):
     name = models.TextField(verbose_name="Cast member name")
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Caption(models.Model):
@@ -31,3 +37,20 @@ class Caption(models.Model):
     text = models.TextField(verbose_name="Caption text")
     lines = ArrayField(models.TextField(name="line"))
 
+    @property
+    def start_ts(self):
+        return self._to_timestamp(self.start)
+
+    @property
+    def end_ts(self):
+        return self._to_timestamp(self.end)
+
+    def _to_timestamp(self, duration):
+        total_seconds = duration.total_seconds()
+        hours = int(total_seconds / 3600)
+        minutes = int(total_seconds / 60 - hours * 60)
+        seconds = total_seconds - hours * 3600 - minutes * 60
+        return "{:02d}:{:02d}:{:06.3f}".format(hours, minutes, seconds)
+
+    def __str__(self):
+        return f"{self.start_ts}-{self.end_ts} {self.text[:30]}..."
