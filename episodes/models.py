17db70ce9009daaadc04c2e1a5f4da29b48281c2
Rodrigo Deodoro <rodrigo.deodoro@esharesinc.com>
2020-10-12 19:26:52 -0700

    start work on episode sections

--- episodes/models.py
+++ episodes/models.py
@@ -1,3 +1,5 @@
+import json
+import datetime
 from urllib.parse import urlencode
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
@@ -39,6 +41,9 @@ class CastMember(models.Model):
         return f"{self.name}"
 
 
+SECTIONS_FILE_PATH = "episodes/management/commands/sections.json"
+
+
 class Caption(models.Model):
     episode = models.ForeignKey(
         "episodes.Episode", related_name="captions", on_delete=models.CASCADE
@@ -47,7 +52,7 @@ class Caption(models.Model):
     duration = models.DurationField(verbose_name="Line length in time")
     start = models.DurationField(verbose_name="Line start timestamp")
     end = models.DurationField(verbose_name="Line end timestamp")
-
+    section = models.TextField(verbose_name="Section identifier", blank=True, null=True)
     text = models.TextField(verbose_name="Caption text")
     lines = ArrayField(models.TextField(name="line"))
 
@@ -85,12 +90,48 @@ class Caption(models.Model):
     def end_ts(self):
         return self._to_timestamp(self.end)
 
-    def _to_timestamp(self, duration):
+    @staticmethod
+    def _to_timestamp(duration):
         total_seconds = duration.total_seconds()
         hours = int(total_seconds / 3600)
         minutes = int(total_seconds / 60 - hours * 60)
         seconds = total_seconds - hours * 3600 - minutes * 60
         return "{:02d}:{:02d}:{:06.3f}".format(hours, minutes, seconds)
 
+    @staticmethod
+    def _from_timestamp(timestamp: str):
+        hours, minutes, seconds = [float(x) for x in timestamp.split(":")]
+        return datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+
     def __str__(self):
         return f"[{self.start_ts}-{self.end_ts}] {self.text[:30]}..."
+
+    @staticmethod
+    def apply_sections():
+        with open(SECTIONS_FILE_PATH) as f:
+            section_data = json.load(f)
+
+        for episode_id, sections in section_data.items():
+            episode = Episode.objects.get(id=episode_id)
+            for section, timestamp in sections.items():
+                if not timestamp:
+                    continue
+                episode.captions.filter(
+                    start=Caption._from_timestamp(timestamp)
+                ).update(section=section)
+
+    def identify(self, section_type):
+        """
+        Section type can be:
+            first_part
+            break
+            second_part
+            ...more?
+        """
+        with open(SECTIONS_FILE_PATH) as f:
+            sections = json.load(f)
+
+        sections[str(self.episode.chapter)][section_type] = self.start_ts
+
+        with open(SECTIONS_FILE_PATH, "w") as f:
+            json.dump(sections, f, indent=2)
